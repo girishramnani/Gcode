505bdfbb45325be1ba2b78203c8940b2d58f1dda
girishramnani <girishramnani95@gmail.com>
2015-07-26 15:02:18 +0530

    added iframe in the right

--- js/main.js
+++ js/main.js
@@ -6,3 +6,38 @@ $().ready(function(){
  });
     
 });
+
+  
+      var  monkeyPatch = function(info){
+          console.log("called");
+       $("#rightPane").toggle();   
+       $(".vsplitbar").toggle();
+          
+      };
+      
+    (function() {
+      // Create a CM instance
+      var ed = CodeMirror.fromTextArea(document.getElementById("code"), {
+        lineNumbers: true,
+        lineWrapping: true,
+        readOnly: false,
+        dragDrop: true,
+        mode : "xml",
+    htmlMode: true,
+      extraKeys: {
+        "F11": function(cm) {
+          cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+          monkeyPatch();
+            
+        },
+        "Esc": function(cm) {
+          if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+             monkeyPatch();
+           
+        }
+      }
+          
+      });
+     
+    })();
+    
