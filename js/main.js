c2cb86b45b3ad1a87278ae5792971a36f494f89d
girishramnani <girishramnani95@gmail.com>
2015-08-05 11:55:28 +0530

    added the run button

--- js/main.js
+++ js/main.js
@@ -4,17 +4,17 @@ $(function() {
 $().ready(function(){
    $("#MySplitter").splitter();
  });
-    
+
 });
 
-  
+
       var  monkeyPatch = function(info){
           console.log("called");
-       $("#rightPane").toggle();   
+       $("#rightPane").toggle();
        $(".vsplitbar").toggle();
-          
+
       };
-      
+
     (function() {
       // Create a CM instance
       var ed = CodeMirror.fromTextArea(document.getElementById("code"), {
@@ -28,16 +28,21 @@ $().ready(function(){
         "F11": function(cm) {
           cm.setOption("fullScreen", !cm.getOption("fullScreen"));
           monkeyPatch();
-            
+
         },
         "Esc": function(cm) {
           if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
              monkeyPatch();
-           
+
         }
       }
-          
+
       });
-     
+      $('.run').click(function (ev) {
+        var content = ed.getValue();
+        var iframe = $(".preview").contents().find('html');
+        iframe.html(content);
+      })
+
+
     })();
-    
